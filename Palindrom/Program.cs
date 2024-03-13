c7cb32de9aafe537a80ec05367782c8c01b88c1e
Dusha_Ada <lola180880mail.ru@gmail.com>
2024-03-13 23:13:01 +0500

    Добавьте файлы проекта.

---
+++ Palindrom/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Введите строку: ");
+        string stroka = Console.ReadLine();
+        Ispalindrom(stroka);
+    }
+    public static void Ispalindrom(string stroka)
+    {
+        string cleanStr = new string(stroka.Where(char.IsLetterOrDigit).ToArray());
+
+        cleanStr = cleanStr.ToLower();
+
+        char[] characters = cleanStr.ToCharArray();
+        Array.Reverse(characters);
+        string backwards = new string(characters);
+
+        if (backwards == cleanStr)
+        {
+            Console.WriteLine("True");
+        }
+        else
+        {
+            Console.WriteLine("False");
+        }
+    }
+}
